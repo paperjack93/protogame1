a39c4e8e74a47e8a79c9adbdb40d0068721b30d8
Stefano Sottocorna <s.sottocorna@gmail.com>
2018-11-06 16:30:29 +0200

    initial commit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+	public LayerMask layerMask;
+	public Rigidbody player;
+	public float playerMoveStrength = 50f;
+	Ray _ray;
+	RaycastHit _hit;
+
+    void Start() {
+        
+    }
+
+    void FixedUpdate() {
+    	Vector3 pos = Input.mousePosition;
+    	if(Input.touches.Length > 0) pos = Input.GetTouch(0).position;
+        _ray = Camera.main.ScreenPointToRay(pos);
+        if (!Physics.Raycast(_ray, out _hit, Mathf.Infinity, layerMask)) return;
+        player.velocity = (_hit.point - player.position) * playerMoveStrength;
+    }
+}
